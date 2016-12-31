ff42a4f7e4ce0d96542f808128ca7302b80893d3
Maxime Martineau <maxime@prafiny.org>
2016-12-31 04:19:24 +0100

    update script adapted for win

--- update.ps1
+++ update.ps1
@@ -12,4 +12,7 @@ $DOCDIR = (Get-Item -Path ".\" -Verbose).FullName
 $TARGETDIR = "$DOCDIR\w32-bin"
 if( -not (Test-Path "$TARGETDIR") ){ New-Item -ItemType directory -Path $TARGETDIR; (New-Object Net.WebClient).DownloadFile('https://sourceforge.net/projects/win-bash/files/shell-complete/latest/shell.w32-ix86.zip/download',"$DOCDIR\win-bash.zip"); Expand-ZIPFile -File "$DOCDIR\win-bash.zip" -Destination "$DOCDIR\w32-bin\"; Remove-Item "$DOCDIR\win-bash.zip"; }
 
-w32-bin\bash.exe update.sh
+(New-Object Net.WebClient).DownloadFile('https://github.com/prafiny/db-project/archive/master.zip',"$DOCDIR\master.zip");
+Expand-ZIPFile -File "$DOCDIR\master.zip" -Destination "$DOCDIR\"; Remove-Item "$DOCDIR\master.zip";
+
+w32-bin\bash.exe update.sh --no-zip
--- update.sh
+++ update.sh
@@ -1,28 +1,19 @@
 #!/bin/bash -e
-files=("autoload.php"
-"composer.json"
-"composer-setup.php"
-"controller"
-"instructions"
-"lib"
-"README.md"
-"server.bat"
-"tests"
-"tests.bat"
-"view"
-"www")
+files="autoload.php composer.json composer-setup.php controller instructions lib README.md tests update.bat update.ps1 update.sh view www"
 
-if [ $# -eq 0 ]; then
-    wget -O master.zip https://github.com/prafiny/db-project/archive/master.zip
-    unzip master.zip
-    for i in "${files[@]}"
+if [ $# -eq 0 ] || [ $1 == "--no-zip" ]; then
+    if [ $1 != "--no-zip" ]; then
+		wget -O master.zip http://github.com/prafiny/db-project/archive/master.zip
+		unzip master.zip
+	fi
+    for i in "${files}"
     do
             rm -r -f $i
     done
     rm -r -f bin
     bash db-project-master/update.sh --new-version
 else
-    for i in "${files[@]}"
+    for i in "${files}"
     do
             cp -r db-project-master/$i .
     done
@@ -32,3 +23,4 @@ else
     php composer-setup.php --install-dir=bin --filename=composer
     php bin/composer update
 fi
+
