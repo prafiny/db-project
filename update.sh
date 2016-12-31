54b152623052ccf3529a9c9e4095198ab407c2a5
Maxime Martineau <maxime@prafiny.org>
2016-12-31 04:24:52 +0100

    replaced zip with tar.gz

--- update.sh
+++ update.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -e
 files="autoload.php composer.json composer-setup.php controller instructions lib README.md tests update.bat update.ps1 update.sh view www"
 
-if [ $# -eq 0 ] || [ $1 == "--no-zip" ]; then
-    if [ $1 != "--no-zip" ]; then
-		wget -O master.zip http://github.com/prafiny/db-project/archive/master.zip
-		unzip master.zip
-	fi
+if [ $# -eq 0 ]; then
+    wget -O master.tar.gz http://github.com/prafiny/db-project/archive/master.tar.gz
+    tar xvf master.tar.gz
     for i in "${files}"
     do
             rm -r -f $i
