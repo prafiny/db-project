f9525e06b6de0bb3b2ecb86d802b6f58d7d9cb55
Maxime Martineau <maxime@prafiny.org>
2016-12-31 05:34:31 +0100

    update.sh for Unix only

--- update.sh
+++ update.sh
@@ -1,20 +1,18 @@
 #!/bin/bash -e
-files="autoload.php composer.json composer-setup.php controller instructions lib README.md tests update.bat update.ps1 update.sh view www"
+files=(autoload.php composer.json composer-setup.php controller instructions lib README.md tests update.bat update.ps1 update.sh view www)
 
-if [ $# -eq 0 ] || [ $1 == "--no-dl" ]; then
-    if [ $1 != "--no-dl" ]; then
-        wget -O master.tar.gz http://github.com/prafiny/db-project/archive/master.tar.gz
-    fi
+if [ $# -eq 0 ]; then
+    wget -O master.tar.gz http://github.com/prafiny/db-project/archive/master.tar.gz
     tar xvf master.tar.gz
     rm master.tar.gz
-    for i in "${files}"
+    for i in "${files[@]}"
     do
             rm -r -f $i
     done
     rm -r -f bin
     bash db-project-master/update.sh --new-version
 else
-    for i in "${files}"
+    for i in "${files[@]}"
     do
             cp -r db-project-master/$i .
     done
