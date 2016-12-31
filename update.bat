4bb42a0ef8a067353e1ff4cd2e271ff5a4a56f8d
Maxime Martineau <maxime@prafiny.org>
2016-12-31 03:58:12 +0100

    scripts update

---
+++ update.bat
@@ -0,0 +1,1 @@
+powershell.exe "Get-Content .\t.ps1 | powershell.exe -noprofile -"
---
+++ update.ps1
@@ -0,0 +1,15 @@
+function Expand-ZIPFile($file, $destination)
+{
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace($file)
+foreach($item in $zip.items())
+{
+$shell.Namespace($destination).copyhere($item)
+}
+}
+
+$DOCDIR = (Get-Item -Path ".\" -Verbose).FullName
+$TARGETDIR = "$DOCDIR\w32-bin"
+if( -not (Test-Path "$TARGETDIR") ){ New-Item -ItemType directory -Path $TARGETDIR; (New-Object Net.WebClient).DownloadFile('https://sourceforge.net/projects/win-bash/files/shell-complete/latest/shell.w32-ix86.zip/download',"$DOCDIR\win-bash.zip"); Expand-ZIPFile -File "$DOCDIR\win-bash.zip" -Destination "$DOCDIR\w32-bin\"; Remove-Item "$DOCDIR\win-bash.zip"; }
+
+w32-bin\bash.exe update.sh
