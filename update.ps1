366f70a4e97261a8b87ec8f38df299ef014c3734
Maxime Martineau <maxime@prafiny.org>
2017-01-02 19:09:59 +0100

    script working for win7

--- update.ps1
+++ update.ps1
@@ -12,9 +12,12 @@ $DOCDIR = (Get-Item -Path ".\" -Verbose).FullName
 (New-Object Net.WebClient).DownloadFile('https://github.com/prafiny/db-project/archive/master.zip',"$DOCDIR\master.zip");
 Expand-ZIPFile -File "$DOCDIR\master.zip" -Destination "$DOCDIR\"; Remove-Item "$DOCDIR\master.zip";
 
-$myArray = autoload.php,composer.json,composer-setup.php,controller,instructions,lib,README.md,tests,update.sh,view,www,update.ps1
+$myArray = "autoload.php","composer.json","composer-setup.php","controller","instructions","lib","README.md","tests","update.sh","view","www","update.ps1"
+$shell = new-object -com shell.application
 foreach($item in $myArray)
 {
+"$item"
 Remove-Item -path $DOCDIR\$item -recurse
-$shell.Namespace($DOCDIR\).copyhere(db-project-master\$item)
+$shell.Namespace($DOCDIR).copyhere("$DOCDIR\db-project-master\$item")
 }
+Remove-Item -path $DOCDIR\db-project-master -recurse
