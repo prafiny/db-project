51469d1c6e3d9b7179816bbb97f4397c6e465522
Maxime Martineau <maxime@prafiny.org>
2017-01-11 19:19:14 +0100

    text in unit

--- tests/notification.php
+++ tests/notification.php
@@ -37,21 +37,21 @@ class NotificationTest extends TestCase
     {
         Post\like(self::$uids[0], self::$pids[1]);
         $n = Notification\get_liked_notifications(self::$uids[1]);
-        $this->assertEquals(count($n), 1);
-        $this->assertEquals($n[0]->post->id, self::$pids[1]);
-        $this->assertEquals($n[0]->liked_by->id, self::$uids[0]);
-        $this->assertEquals($n[0]->type, "liked");
-        $this->assertObjectHasAttribute('date', $n[0]);
-        $this->assertObjectHasAttribute('reading_date', $n[0]);
+        $this->assertEquals(count($n), 1, "get_liked_notifications should return an array of liked notifications");
+        $this->assertEquals($n[0]->post->id, self::$pids[1], "post in 'liked' notification should be the one liked");
+        $this->assertEquals($n[0]->liked_by->id, self::$uids[0], "liked_by in 'liked' notification should be the user who liked the post");
+        $this->assertEquals($n[0]->type, "liked", "the notification should have a type attribute equal to 'liked'");
+        $this->assertObjectHasAttribute('date', $n[0], "the liked notification should have a date attribute");
+        $this->assertObjectHasAttribute('reading_date', $n[0], "the liked notification should have a reading_date attribute");
 
-        $this->assertEquals($n[0]->reading_date, null);
-        $this->assertTrue(Notification\liked_notification_seen(self::$pids[1], self::$uids[0]));
+        $this->assertNull($n[0]->reading_date, "reading_date should be equal to null if the notification hasn't been seen");
+        $this->assertTrue(Notification\liked_notification_seen(self::$pids[1], self::$uids[0]), "liked_notification_seen should return true if everything went ok");
         $n = Notification\get_liked_notifications(self::$uids[1]);
-        $this->assertNotEquals($n[0]->reading_date, null);
+        $this->assertNotNull($n[0]->reading_date, "reading date from get_liked_notifications shouldn't be null if the notification has been seen");
         
         Post\unlike(self::$uids[0], self::$pids[1]);
         $n = Notification\get_liked_notifications(self::$uids[1]);
-        $this->assertEmpty($n);
+        $this->assertEmpty($n, "get_liked_notification should not return a notification if a like has been undone");
     }
 
     /**
@@ -61,21 +61,21 @@ class NotificationTest extends TestCase
     {
         Post\mention_user(self::$pids[0], self::$uids[1]);
         $n = Notification\get_mentioned_notifications(self::$uids[1]);
-        $this->assertEquals(count($n), 1);
-        $this->assertEquals($n[0]->mentioned_by->id, self::$uids[0]);
-        $this->assertEquals($n[0]->post->id, self::$pids[0]);
-        $this->assertEquals($n[0]->type, "mentioned");
-        $this->assertObjectHasAttribute('date', $n[0]);
+        $this->assertEquals(count($n), 1 , "get_mentioned_notifications should return an array of mentioned notifications");
+        $this->assertEquals($n[0]->mentioned_by->id, self::$uids[0], "mentioned_by should be the person who created the post in which the user was mentioned");
+        $this->assertEquals($n[0]->post->id, self::$pids[0], "post should be the post in which the user was mentioned");
+        $this->assertEquals($n[0]->type, "mentioned", "the notification should have a type attribute equal to 'mentioned'");
+        $this->assertObjectHasAttribute('date', $n[0], "the mentioned notification should have a date attribute");
         $this->assertObjectHasAttribute('reading_date', $n[0]);
 
-        $this->assertEquals($n[0]->reading_date, null);
+        $this->assertNull($n[0]->reading_date, "reading_date should be equal to null if the notification hasn't been seen");
         $this->assertTrue(Notification\mentioned_notification_seen(self::$uids[1], self::$pids[0]));
         $n = Notification\get_mentioned_notifications(self::$uids[1]);
-        $this->assertNotEquals($n[0]->reading_date, null);
+        $this->assertNotNull($n[0]->reading_date, "reading date from get_mentioned_notifications shouldn't be null if the notification has been seen");
 
         Post\destroy(self::$pids[0]);
         $n = Notification\get_mentioned_notifications(self::$uids[1]);
-        $this->assertEmpty($n);
+        $this->assertEmpty($n, "get_mentioned_notification should not return a notification if the post where the mention was has been destroyed");
     }
 
     /**
@@ -85,19 +85,19 @@ class NotificationTest extends TestCase
     {
         User\follow(self::$uids[0], self::$uids[1]);
         $n = Notification\get_followed_notifications(self::$uids[1]);
-        $this->assertEquals(count($n), 1);
-        $this->assertEquals($n[0]->user->id, self::$uids[0]);
-        $this->assertEquals($n[0]->type, "followed");
-        $this->assertObjectHasAttribute('date', $n[0]);
+        $this->assertEquals(count($n), 1, "get_followed_notifications should return an array of followed notifications");
+        $this->assertEquals($n[0]->user->id, self::$uids[0], "user should be the person who followed the user");
+        $this->assertEquals($n[0]->type, "followed", "the followed notification should have a type attribute equal to 'followed'");
+        $this->assertObjectHasAttribute('date', $n[0], "the followed notification should have a date attribute");
 
-        $this->assertEquals($n[0]->reading_date, null);
+        $this->assertNull($n[0]->reading_date, "reading_date should be equal to null if the notification hasn't been seen");
         $this->assertTrue(Notification\followed_notification_seen(self::$uids[1], self::$uids[0]));
         $n = Notification\get_followed_notifications(self::$uids[1]);
-        $this->assertNotEquals($n[0]->reading_date, null);
+        $this->assertNotNull($n[0]->reading_date, "reading date from get_followed_notifications shouldn't be null if the notification has been seen");
         
         User\unfollow(self::$uids[0], self::$uids[1]);
         $n = Notification\get_followed_notifications(self::$uids[1]);
-        $this->assertEmpty($n);
+        $this->assertEmpty($n, "get_followed_notification should not return a notification if the other user is not following anymore");
     }
 
     /**
@@ -113,9 +113,9 @@ class NotificationTest extends TestCase
         
         $n = Notification\list_all_notifications(self::$uids[1]);
         $this->assertEquals(count($n), 3);
-        $this->assertEquals("mentioned", $n[2]->type);
-        $this->assertEquals("followed", $n[1]->type);
-        $this->assertEquals("liked", $n[0]->type);
+        $this->assertEquals("mentioned", $n[2]->type, "list_all notifications should be able to sort notifications by dates (with datetime objects)");
+        $this->assertEquals("followed", $n[1]->type, "list_all notifications should be able to sort notifications by dates (with datetime objects)");
+        $this->assertEquals("liked", $n[0]->type, "list_all notifications should be able to sort notifications by dates (with datetime objects)");
     }
 
     public static function tearDownAfterClass()
