22c3b24565d5d0e77e0737d7a5b7e48c41ce0102
Maxime Martineau <maxime@prafiny.org>
2017-11-23 02:53:01 +0100

    populatedb refact

--- scripts/populate_db.sh
+++ scripts/populate_db.sh
@@ -13,12 +13,12 @@ entries_file="sql/entries.sql"
 $(python3 scripts/get_yaml.py "config/db.yaml" "$1")
 mysql_cmd="-h$server -u$username -p$password"
 if [ "$local_database" = "true" ]; then
-	bash scripts/purge_db.sh "$1"	
-	mysqladmin $mysql_cmd create "$db"
 	cat "$schemas_file" | awk '!/^(use|create database|drop database|USE|CREATE DATABASE|DROP DATABASE)/' > buf.sql
         if [ "$1" = "app" ]; then
             cat "$entries_file" | awk '!/^(use|create database|drop database|USE|CREATE DATABASE|DROP DATABASE)/' >> buf.sql
         fi
+	bash scripts/purge_db.sh "$1"	
+	mysqladmin $mysql_cmd create "$db"
 	mysql $mysql_cmd $db < buf.sql
 	rm buf.sql
 else
