6aac53ecab1444d728a92fdb3c5911ee1ac08939
Maxime Martineau <maxime@prafiny.org>
2017-11-23 15:47:38 +0100

    added user model for avatar handling

---
+++ model/user.php
@@ -0,0 +1,13 @@
+<?php
+namespace Model\User;
+
+function handle_avatar($filename, $tmpfilename, $username) {
+    $allowed =  array('gif','png' ,'jpg');
+    $ext = \pathinfo($filename, \PATHINFO_EXTENSION);
+    if(!in_array($ext,$allowed) ) {
+        return null;
+    }
+    $newname = 'images/'.$username.'.'.$ext;
+    move_uploaded_file($tmpfilename, $newname);
+    return $newname;
+}
