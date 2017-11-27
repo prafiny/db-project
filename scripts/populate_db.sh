6b2e8d65d85af1bc07d8662ce4cb30d006eb2d36
Maxime Martineau <maxime@prafiny.org>
2017-11-27 12:29:37 +0100

    fixed populate_db options

--- scripts/populate_db.sh
+++ scripts/populate_db.sh
@@ -5,21 +5,22 @@ source scripts/helpers/macros.sh
 check_term
 
 if [ $# -lt 1 ]; then
-    echo "USAGE : ./purge_db.sh env"
-    exit
+    env=app
+else
+    env=$1
 fi
 
 schemas_file="sql/schemas.sql"
 entries_file="sql/entries.sql"
 
-$(python3 scripts/get_yaml.py "config/db.yaml" "$1")
+$(python3 scripts/get_yaml.py "config/db.yaml" "$env")
 mysql_cmd="-h$server -u$username -p$password"
 if [[ -v LOCAL_DBPROJECT ]]; then
 	cat "$schemas_file" | awk '!/^(use|create database|drop database|USE|CREATE DATABASE|DROP DATABASE)/' > buf.sql
-        if [ "$1" = "app" ]; then
+        if [ "$env" = "app" ]; then
             cat "$entries_file" | awk '!/^(use|create database|drop database|USE|CREATE DATABASE|DROP DATABASE)/' >> buf.sql
         fi
-	bash scripts/purge_db.sh "$1"	
+	bash scripts/purge_db.sh "$env"
 	mysqladmin $mysql_cmd create "$db"
 	mysql $mysql_cmd $db < buf.sql
 	rm buf.sql
