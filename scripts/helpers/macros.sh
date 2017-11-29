cc7ae2c5ca0e95f72a93516fdf38d47e0a473c7f
Maxime Martineau <maxime@prafiny.org>
2017-11-29 17:38:02 +0100

    compat add

--- scripts/helpers/macros.sh
+++ scripts/helpers/macros.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 check_term() {
-if [[ ! -v NO_TERM ]]; then
+if [ -z "$NO_TERM" ]; then
 	if [ ! -t 0 ]; then
 		xterm -hold -e "bash $0; echo; echo Terminated. You can close the window."
 	fi
