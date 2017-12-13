808a3c283dba10339cd187d7efc6814fa6172239
Maxime Martineau <maxime@prafiny.org>
2017-12-13 13:59:02 +0100

    fixed populate_db

--- scripts/populate_db.sh
+++ scripts/populate_db.sh
@@ -9,12 +9,6 @@ case "$1" in
   * ) env="app";;
 esac
 
-if [ $# -lt 1 ] || [ -z "$1" ] || [ "$1" != "test" ]; then
-    env="app"
-else
-    env="test"
-fi
-
 schemas_file="sql/schemas.sql"
 entries_file="sql/entries.sql"
 
@@ -22,9 +16,9 @@ $(python3 scripts/get_yaml.py "config/db.yaml" "$env")
 mysql_cmd="-h$server -u$username -p$password"
 if [ -n "$LOCAL_DBPROJECT" ]; then
 	backup "last_is_file"
-        cat "$schemas_file" | awk '!/^(use|create database|drop database|USE|CREATE DATABASE|DROP DATABASE)/' > buf.sql
+        cat "$schemas_file.sql" | awk '!/^(use|create database|drop database|USE|CREATE DATABASE|DROP DATABASE)/' > buf.sql
         if [ "$env" = "app" ]; then
-            cat "$entries_file" | awk '!/^(use|create database|drop database|USE|CREATE DATABASE|DROP DATABASE)/' >> buf.sql
+            cat "$entries_file.sql" | awk '!/^(use|create database|drop database|USE|CREATE DATABASE|DROP DATABASE)/' >> buf.sql
         fi
 	bash scripts/purge_db.sh "$env"
 	mysqladmin $mysql_cmd create "$db"
