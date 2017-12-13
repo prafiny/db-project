51fcd1c0656781dc747a9646abfc2c6766327ba9
Maxime Martineau <maxime@prafiny.org>
2017-12-13 13:37:21 +0100

    test with arg

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -4,9 +4,17 @@ cd "$SCRIPTPATH/../"
 source scripts/helpers/macros.sh
 check_term
 
+
 if [ -n "$LOCAL_DBPROJECT" ]; then
+case "$1" in 
+  user) f="user" ;;
+  post) f="post" ;;
+  hashtag) f="hashtag" ;;
+  notification) f="notification" ;;
+  * ) f="";;
+esac
 	bash scripts/populate_db.sh "test"
-	for i in "tests/"*; do
+	for i in "tests/"*"${f}.php"; do
 		echo $i
 		echo -------
 		phpunit --bootstrap "autoload.php" "$i"
