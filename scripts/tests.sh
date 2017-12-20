a9ab7d5dfa7da5624f6745078324cf037d04f56c
Maxime Martineau <maxime@prafiny.org>
2017-12-20 17:22:03 +0100

    fixed bug tests

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -13,11 +13,14 @@ case "$1" in
   notification) f="notification" ;;
   * ) f="";;
 esac
-	bash scripts/populate_db.sh "test"
+	NO_TERM="true" bash scripts/populate_db.sh "test"
 	for i in "tests/"*"${f}.php"; do
 		echo $i
 		echo -------
 		phpunit --bootstrap "autoload.php" "$i"
+                echo
+                echo
+                echo
 	done
 else
 	vagrant ssh -c "export LOCAL_DBPROJECT=true; bash /vagrant/scripts/tests.sh"
