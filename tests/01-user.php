c6c5ac587db8f5b5591206c1c4545c43d9955ed4
Maxime Martineau <maxime@prafiny.org>
2017-12-20 18:21:47 +0100

    removed assertTrue for array inclusion

--- tests/01-user.php
+++ tests/01-user.php
@@ -100,8 +100,8 @@ class UserTest extends TestCase
         $new_user = User\get($new_uid);
         $l = User\list_all();
         $this->assertEquals(2, count($l), "list_all should list every user");
-        $this->assertTrue(in_array($new_user, $l), "list_all should list every user in user objects");
-        $this->assertTrue(in_array($existing_user, $l), "list_all should list every user in user objects");
+        $this->assertContains($new_user, $l, "list_all should list every user in user objects");
+        $this->assertContains($existing_user, $l, "list_all should list every user in user objects");
         return [$existing_user, $new_user];
     }
 
